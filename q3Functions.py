a6dd28677b6eab9eb3f48e6060b2fd020eb7e7a8
HarshitAnand1 <harshitaanaand@gmail.com>
2024-09-02 20:21:57 +0530

    q3Functions.py

--- q3Functions.py
+++ q3Functions.py
@@ -1,14 +1,16 @@
-def perfect_num_checker():
-    n = int(input("Enter a number: "))
-
-    div_sum = 0
-    for i in range (1, n//2 + 1):
-        if n//i == n/i:
-            div_sum = div_sum +i
+def perfect_num_checker(): 
+    num_list = [6, 2, 20, 496, 30, 8128, 500, 1000, 33550336, 999983]
     
-    if div_sum == n:
-        print("The given number is a pefect number")
-    else:
-        print("The given number is not a pefect number")
-
+    for num in num_list: 
+        div_sum = 0
+        
+        for i in range(1,((num//2)+1)): 
+            if num%i==0:
+                div_sum = div_sum +i
+                
+        if div_sum == num: 
+            print(f"{num} is a pefect number") 
+        else: 
+            print(f"{num} is not a pefect number")
+            
 perfect_num_checker()
